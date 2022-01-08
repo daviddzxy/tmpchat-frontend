f01b288425ff068ad3cc507f115945e2fe471df5
daviddzxy <david.dzxy@gmail.com>
2022-01-08 17:50:09 +0100

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Home from "./Home.js";
+import Chat from "./Chat"
 import React from "react";
 import {createGlobalStyle} from "styled-components"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -12,7 +14,12 @@ const App = () => {
     return (
         <React.Fragment>
             <GlobalStyle/>
-            <Home/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/chat/:name" element={<Chat />}/>
+                </Routes>
+            </BrowserRouter>
         </React.Fragment>
     );
 }
